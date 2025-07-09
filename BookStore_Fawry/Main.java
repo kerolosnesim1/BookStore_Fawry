f9d9c78cdc0f20cba41c3c1458898fb9f00d6b61
kerolos nesim <112533022+kerolosnesim1@users.noreply.github.com>
2025-07-09 04:21:17 +0300

    Add missing remove outdated books function Main.java

--- BookStore_Fawry/Main.java
+++ BookStore_Fawry/Main.java
@@ -17,10 +17,18 @@ public class Main {
     PaperBook book1 = new PaperBook("book1", "java stuff", 2020, 25.0, "some guy", 5);
     EBook book2 = new EBook("book2", "python thing", 2021, 20.0, "mary", "pdf");
     ShowcaseBook book3 = new ShowcaseBook("book3", "demo", 2023, 50.0, "bob");
+    PaperBook book4 = new PaperBook("book4", "old outdated book", 2019, 15.0, "old author", 3);
 
     store.addBook(book1);
     store.addBook(book2);
     store.addBook(book3);
+    store.addBook(book4);
+
+    System.out.println();
+    store.removeOutdatedBooks(4);
+    System.out.println("old outdated book removed since it's outdated");
+    System.out.println();
+
 
     System.out.println("here are the books:");
     store.displayInventory();
@@ -32,4 +40,5 @@ public class Main {
     System.out.println("books now:");
     store.displayInventory();
   }
+}
 }
